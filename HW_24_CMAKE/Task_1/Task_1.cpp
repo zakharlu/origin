2ce1a573dba7cadccc18e36572de336e6dcd0b0e
leonid_z <plcard@bk.ru>
2025-03-30 22:47:30 +0300

    Ex24_Cmake

---
+++ HW_24_CMAKE/Task_1/Task_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << "Здравствуйте, " << name;
+    return 0;
+}
