7cc829b421af7f8489ee8fe63a9a7e4f797190e7
zakharlu <zakharlu@gmail.com>
2025-04-12 20:47:37 +0300

    Add files via upload

---
+++ HW_25/Task_2/SRC/SRC.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Leaver.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Leaver leaver;
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << leaver.leave(name) << std::endl;
+
+    return 0;
+}
