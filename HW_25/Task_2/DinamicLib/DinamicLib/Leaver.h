3e9ac570598ac0c9311d6482f8c61baafa82ae57
zakharlu <zakharlu@gmail.com>
2025-04-14 14:13:42 +0300

    Add files via upload

---
+++ HW_25/Task_2/DinamicLib/DinamicLib/Leaver.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*#ifdef LIB_DYNAMIC_EXPORTS
+#define LIB_DYNAMIC_API __declspec(dllexport)
+#else
+#define LIB_DYNAMIC_API __declspec(dllimport)
+#endif
+*/
+class Leaver
+{
+public:
+	//LIB_DYNAMIC_API std::string leave(std::string str);
+	std::string leave(std::string str);
+};
