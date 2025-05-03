71922981c3c814cbd4c63f24468a1e23bb165c8a
leonid_z <plcard@bk.ru>
2025-05-04 00:23:17 +0300

    Fibonacci

---
+++ HW29_Dinamyc_prog/Task_1/Task_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int fibonacci(int n);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    std::cout << "Введите номер числа Фибоначчи: ";
+    std::cin >> n;
+    int fib = 0;
+    fib = fibonacci(n);
+    std::cout << "Значение Фибоначчи: " << fib << std::endl;
+
+}
+
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
+}
---
+++ HW29_Dinamyc_prog/Task_2/Task_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int fibonacci(int n);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    std::cout << "Введите номер числа Фибоначчи от 0 до 9: ";
+    std::cin >> n;
+    int fib = 0;
+    fib = fibonacci(n);
+    std::cout << "Значение Фибоначчи: " << fib << std::endl;
+
+}
+
+int fibonacci(int n) {
+    int arr[10] = { 0 };
+    if (n <= 1) return n;
+    else {
+        if (arr[n] != 0) {
+            return arr[n];
+        }
+        arr[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
