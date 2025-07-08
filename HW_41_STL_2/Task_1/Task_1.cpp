83a0363b89082449ba18d809278bfdbe3132004a
leonid_z <plcard@bk.ru>
2025-07-08 21:27:19 +0300

    stl_2

---
+++ HW_41_STL_2/Task_1/Task_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <algorithm>
+
+template<typename T>
+void remove_duplicates(std::vector<T>& vec) {
+    // Сортируем вектор
+    std::sort(vec.begin(), vec.end());
+
+    // Удаляем подряд идущие дубликаты
+    auto last = std::unique(vec.begin(), vec.end());
+
+    // Удаляем оставшиеся элементы
+    vec.erase(last, vec.end());
+}
+
+#include <iostream>
+
+int main() {
+    std::vector<int> vec = {1, 1, 2, 5, 6, 1, 2, 4};
+
+    remove_duplicates(vec);
+
+    for (const auto& x : vec) {
+        std::cout << x << " ";
+    }
+
+    return 0;
+}
---
+++ HW_41_STL_2/Task_2/Task_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+template <typename Container>
+void print_container(const Container& container) {
+    bool is_first = true; 
+    for (const auto& item : container) {
+        if (!is_first) {
+            std::cout << ", ";
+        }
+        std::cout << item;
+        is_first = false;  
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    return 0;
+}
