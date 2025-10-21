4d6105a7ffa87e91765842c8b83dda38aeffed87
leonid_z <plcard@bk.ru>
2025-10-21 22:28:44 +0300

    patterns

---
+++ HW_57_Structure_Patterns/Task_1/Task_1.cpp
@@ -0,0 +1,109 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class SimpleText
+{
+public:
+    virtual void render(const string& text)
+    {
+        cout << text;
+    }
+};
+
+class DecoratedText : public SimpleText
+{
+public:
+    SimpleText* __text;
+    DecoratedText(SimpleText* text) : __text(text) {}
+};
+
+class ItalicText : public DecoratedText
+{
+public:
+    ItalicText(SimpleText* text) : DecoratedText(text) {}
+    void render(const string& text) override
+    {
+        cout << "<i>";
+        __text->render(text);
+        cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText
+{
+public:
+    BoldText(SimpleText* text) : DecoratedText(text) {}
+    void render(const string& text) override
+    {
+        cout << "<b>";
+        __text->render(text);
+        cout << "</b>";
+    }
+};
+
+class Paragraph : public DecoratedText
+{
+public:
+    Paragraph(SimpleText* text) : DecoratedText(text) {}
+    void render(const string& text) override
+    {
+        cout << "<p>";
+        __text->render(text);
+        cout << "</p>";
+    }
+};
+
+class Reversed : public DecoratedText
+{
+public:
+    Reversed(SimpleText* text) : DecoratedText(text) {}
+    void render(const string& text) override
+    {
+        string reversed_text = text;
+        reverse(reversed_text.begin(), reversed_text.end());
+        __text->render(reversed_text);
+    }
+};
+
+class Link : public DecoratedText
+{
+public:
+    Link(SimpleText* text) : DecoratedText(text) {}
+    void render(const string& href, const string& text)
+    {
+        cout << "<a href=" << href << ">";
+        __text->render(text);
+        cout << "</a>";
+    }
+};
+
+int main()
+{
+    SimpleText text1;
+    text1.render("Hello HTML");
+    cout << endl;
+
+    auto italic_text = new ItalicText(&text1);
+    auto bold_text = new BoldText(&text1);
+    italic_text->render("Italic HTML");
+    cout << endl;
+    bold_text->render("Bold HTML");
+    cout << endl;
+
+    // Новые декораторы
+    auto paragraph = new Paragraph(&text1);
+    paragraph->render("Hello world");
+    cout << endl;
+
+    auto reversed = new Reversed(&text1);
+    reversed->render("Hello world");
+    cout << endl;
+
+    auto link = new Link(&text1);
+    link->render("netology.ru", "Hello world");
+    cout << endl;
+
+    return 0;
+}
---
+++ HW_57_Structure_Patterns/Task_2/Task_2.cpp
@@ -0,0 +1,93 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class VeryHeavyDatabase {
+public:
+    virtual std::string get_data(const std::string& key) {
+        return "value";
+    }
+};
+
+class OneShotDB : public VeryHeavyDatabase {
+private:
+    VeryHeavyDatabase* real_object;
+    size_t shots;
+    size_t counter;
+
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_object(real_object), shots(shots), counter(0) {
+    }
+
+    std::string get_data(const std::string& key) override {
+        if (counter < shots) {
+            counter++;
+            return real_object->get_data(key);
+        }
+        else {
+            return "error";
+        }
+    }
+};
+
+class CacheDatabase : public VeryHeavyDatabase {
+    map<string, string> __cache;
+    unique_ptr<VeryHeavyDatabase> __real_db;
+
+public:
+    CacheDatabase(unique_ptr<VeryHeavyDatabase> real_db) : __real_db(move(real_db)) {}
+
+    std::string get_data(const std::string& key) override {
+        if (__cache.find(key) == __cache.end()) {
+            cout << "Get from real DB" << endl;
+            __cache[key] = __real_db->get_data(key);
+        }
+        else {
+            cout << "Get from cache" << endl;
+        }
+        return __cache[key];
+    }
+};
+
+class MockDatabase : public VeryHeavyDatabase {
+    unique_ptr<VeryHeavyDatabase> __real_db;
+
+public:
+    MockDatabase(unique_ptr<VeryHeavyDatabase> real_db) : __real_db(move(real_db)) {}
+
+    std::string get_data(const std::string& key) override {
+        return "Correct data";  // Всегда возвращает "Correct data"
+    }
+};
+
+unique_ptr<CacheDatabase> init_cache() {
+    auto original_DB = make_unique<VeryHeavyDatabase>();
+    return make_unique<CacheDatabase>(move(original_DB));
+}
+
+int main()
+{
+    // Тестирование OneShotDB
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.get_data("key") << std::endl;
+    std::cout << limit_db.get_data("key") << std::endl;
+    std::cout << limit_db.get_data("key") << std::endl;
+
+    cout << endl;
+
+    // Используем MockDatabase для получения "Correct data"
+    auto test_db = make_unique<CacheDatabase>(move(make_unique<MockDatabase>(make_unique<VeryHeavyDatabase>())));
+    cout << test_db->get_data("key2") << endl;
+    cout << test_db->get_data("key3") << endl;
+    cout << test_db->get_data("key4") << endl;
+    cout << test_db->get_data("key5") << endl;
+    cout << test_db->get_data("key6") << endl;
+    cout << test_db->get_data("key7") << endl;
+
+    return 0;
+}
